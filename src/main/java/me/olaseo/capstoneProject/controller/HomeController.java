47a0a6fb99d6f715a731cf29e4e2808c2b7a43ee
olaseo <olaseo@naver.com>
2025-09-20 19:55:07 +0900

    bug fixed

--- src/main/java/me/olaseo/capstoneProject/controller/HomeController.java
+++ src/main/java/me/olaseo/capstoneProject/controller/HomeController.java
@@ -1,4 +1,14 @@
 package me.olaseo.capstoneProject.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+
+        return "home";
+    }
 }
